fe82890bb72cbe4bb0dfd06b6c5c6bfeb0fc6854
Samuel Cornet <samuel@noukod.com>
2018-09-26 15:12:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,26 @@
 class School
+  attr_accessor :roster, :student_name, :grade, :schl_name
 
-  def initialize(name)
-      @name = name
+  def initialize(schl_name)
+    @schl_name = schl_name
+    @roster = {}
   end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(student_name)
+  #   grade.each do |student, grd|
+  #     puts student
+  #   end
+  # end
+  @roster[student_name]
+end
 
 end
